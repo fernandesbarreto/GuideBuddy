f02ffb2c0e794eb1bfbb4786e34cbada1536a1a7
fernandesbarreto <fernandespedrobc@gmail.com>
2024-08-26 17:48:46 -0300

    Initial Commit

---
+++ GuideBuddy/GuideBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBuddyApp.swift
+//  GuideBuddy
+//
+//  Created by Pedro Fernandes Barreto Costa on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
