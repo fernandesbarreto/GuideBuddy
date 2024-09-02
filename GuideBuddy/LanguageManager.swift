b737225009e6571b128e78fb82831a090605b952
Raquel Ribeiro <raquelribeirohf@gmail.com>
2024-09-02 08:45:34 -0300

    Primeira tela sem animação

--- GuideBuddy/LanguageManager.swift
+++ GuideBuddy/LanguageManager.swift
@@ -5,41 +5,25 @@
 //  Created by Raquel Ribeiro Hatem de Farias on 28/08/24.
 //
 
-import Foundation
-
-class LanguageManager: ObservableObject {
-    @Published var currentLanguage: String {
-        didSet {
-            UserDefaults.standard.set(currentLanguage, forKey: "currentLanguage")
-            Bundle.setLanguage(currentLanguage)
-            NotificationCenter.default.post(name: NSNotification.Name("LanguageChanged"), object: nil)
-        }
-    }
-    
-    init() {
-        currentLanguage = UserDefaults.standard.string(forKey: "currentLanguage") ?? Locale.current.language.languageCode?.identifier ?? "en"
-    }
-}
-
-extension Bundle {
-    private static var onLanguageDispatchOnce: Void = {
-        object_setClass(Bundle.main, BundleEx.self)
-    }()
-    
-    class func setLanguage(_ language: String) {
-        Bundle.onLanguageDispatchOnce
-        objc_setAssociatedObject(Bundle.main, &kBundleKey, language, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-    }
-}
-
-private var kBundleKey: UInt8 = 0
-
-private class BundleEx: Bundle {
-    override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
-        guard let bundlePath = objc_getAssociatedObject(self, &kBundleKey) as? String,
-              let bundle = Bundle(path: Bundle.main.path(forResource: bundlePath, ofType: "lproj") ?? "") else {
-            return super.localizedString(forKey: key, value: value, table: tableName)
-        }
-        return bundle.localizedString(forKey: key, value: value, table: tableName)
-    }
-}
+//import Foundation
+//import SwiftUI
+//
+//class LanguageManager: ObservableObject {
+//    @Published var currentLanguage: String = "pt-BR" 
+//
+//    func setLanguage(_ language: String) {
+//        print("abobora")
+//        currentLanguage = language
+//        UserDefaults.standard.set([language], forKey: "AppleLanguages")
+//        UserDefaults.standard.synchronize()
+//    }
+//    
+//    func localizedString(for key: String) -> String {
+//        guard let path = Bundle.main.path(forResource: currentLanguage, ofType: "lproj"),
+//              let bundle = Bundle(path: path) else {
+//            return NSLocalizedString(key, comment: "")
+//        }
+//        return NSLocalizedString(key, tableName: nil, bundle: bundle, value: "", comment: "")
+//    }
+//}
+//
--- GuideBuddy/Login.swift
+++ GuideBuddy/Login.swift
@@ -12,71 +12,101 @@ struct Login: View {
     
     @State private var name = ""
     @State private var age: Double?
-    @StateObject var languageManager = LanguageManager()
+    @State private var languages: Int = 0
+    @State var language = 0
+    @State private var failedInput = false
+    
+    let fillData = ["Preencha os campos", "Fill in your details", "Rellena tus datos"]
+    
+    let flags = ["portugues", "ingles", "espanhol"]
+    let languageCodes = ["pt-BR", "en", "es"]
+    let firstText = ["Vamos lá", "Let's go", "Vamos alla"]
+    let secondText = ["Preencha seus dados", "Fill in your details", "Rellena tus datos"]
+    let thirdText = ["Nome do usuário", "User name", "Nombre de usuario"]
+    let fourthText = ["Idade", "Age", "Edad"]
+    let fifthText = ["Começar", "Start", "Comezar"]
     
     var body: some View {
         
-        GeometryReader { geometry in
+        NavigationStack{
             
-            
-            HStack {
+            GeometryReader { geometry in
                 
-                Spacer()
-                
-                VStack (alignment: .leading){
+                VStack {
+                    Image("GuideBuddy")
+                        .position(x: geometry.size.width/2, y: geometry.size.height/3.5)
                     
-                    Spacer()
-                    Text("lets_go".localized)
-//                    .position(y: geometry.size.width/2)
-
-                    Text("fill_your_details".localized)
-                    //.position(x: geometry.size.width/2.45, y: geometry.size.height/1.82)
+                    Text(firstText[language])
+                        .position(x: geometry.size.width/6.5 ,y: geometry.size.height/2.5)
                     
-                        TextField("user_name".localized, text: $name)
+                    Text(secondText[language])
+                        .position(x: geometry.size.width/3.6, y: geometry.size.height/3.5)
+                    
+                    TextField(thirdText[language], text: $name)
                         .padding(.leading)
-                            .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.05)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .fill(Color.barraTexto)
-                            )
-                        //.position(x: geometry.size.width/2, y: geometry.size.height/1.6)
-                   
-                        
-                    TextField("user_age".localized, value: $age, format: .number)
-                        .padding(.leading)
-                        .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.05)
+                        .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.06)
                         .background(
                             RoundedRectangle(cornerRadius: 8)
                                 .fill(Color.barraTexto)
                         )
-                    //.position(x: geometry.size.width/2, y: geometry.size.height/1.45)
+                        .position(x: geometry.size.width/2, y: geometry.size.height/5)
+                    
+                    TextField(fourthText[language], value: $age, format: .number)
+                        .padding(.leading)
+                        .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.06)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color.barraTexto)
+                        )
+                        .position(x: geometry.size.width/2, y: geometry.size.height/7.5)
                     
                     Button {
-                       
+                        if name.isEmpty || age == nil {
+                            failedInput = true
+                        } else {
+                            // Navigate to the next screen
+                        }
                     } label: {
-                        Text("Começar")
-                            .foregroundStyle(.white)
-                            .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.07)
-                            .background(.verde)
-                            .cornerRadius(14)
+                        NavigationLink(destination: Testinho()) {
+                            Text(fifthText[language])
+                                .foregroundStyle(.white)
+                                .frame(width: geometry.size.width*0.88, height: geometry.size.height*0.07)
+                                .background((name.isEmpty || age == nil) ? Color.gray : Color.verde)
+                                .background(Color.verde)
+                                .cornerRadius(14)
+                        }
+                        .disabled(name.isEmpty || age == nil)
                     }
+                    .alert(fillData[language],
+                           isPresented: $failedInput,
+                           actions: {
+                        Button("OK", role: .cancel, action: {})
+                    })
+                    .position(x: geometry.size.width/2, y: geometry.size.height/8.5)
                     
-                    
-                    Rectangle()
-                        .frame(width: 10, height: 20)
-//
+                    HStack {
+                        Button {
+                            languages = (languages - 1 + flags.count) % flags.count
+                            language = (language - 1 + 3) % 3
+                        } label: {
+                            Image(systemName: "chevron.left")
+                        }
+                        
+                        Image(flags[languages])
+                        
+                        Button {
+                            languages = (languages + 1) % flags.count
+                            language = (language + 1 + 3) % 3
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .position(x: geometry.size.width/2, y: geometry.size.height/11)
                 }
                 
                 Spacer()
-                
             }
-            
-            
-            
-            
         }
-        
-        
     }
 }
 
@@ -89,3 +119,4 @@ extension String {
 #Preview {
     Login()
 }
+
