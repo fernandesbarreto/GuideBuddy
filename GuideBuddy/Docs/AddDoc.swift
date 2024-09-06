2979494a4239dba2238fecaf063d32fa8d1de427
Deivson Pereira da Silva <deivson.pereira@ufpe.br>
2024-09-06 10:24:53 -0300

    Consertei com Juliano os bugs para importar PDF

--- GuideBuddy/Docs/AddDoc.swift
+++ GuideBuddy/Docs/AddDoc.swift
@@ -21,7 +21,7 @@ struct AdicionarDocumento: View {
     @State private var fileURL: URL? = nil // Armazena a URL do arquivo importado
     @State private var fileContent: String = "" // Armazena o conteúdo do arquivo
     
-  
+    
     @State private var items: [Item] = []
     
     var body: some View {
@@ -49,12 +49,14 @@ struct AdicionarDocumento: View {
                         .clipShape(RoundedRectangle(cornerRadius: 12.0))
                         .foregroundColor(.white)
                         
+                        
                         ForEach(Array(items.enumerated()), id: \.element) { index, item in
                             // nesse momento, eu posso fazer o que eu quiser com o item
                             if item.type == 0 {
+                                
                                 if let uiImage = item.selectedImage {
+                                    
                                     Button(action: {
-                                        self.items.remove(at: index)
                                     }) {
                                         Image(uiImage: uiImage)
                                             .resizable()
@@ -63,69 +65,83 @@ struct AdicionarDocumento: View {
                                             .clipped()
                                             .cornerRadius(10)
                                     }
-                                } else {
-                                    Text("teste")
                                 }
-                            } else {
-                                // aqui eu vou desenhar o PDF
+                                
+                            }
+                            else if item.type == 1 {
+                                Image("pdfImage")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 100, height: 100)
+                                    .clipped()
+                                    .cornerRadius(10)
                             }
                         }
+                        
                     }
                 }
-                
-//                if let fileURL = fileURL {
-//                    Text("Arquivo Importado: \(fileURL.lastPathComponent)")
-//                        .padding()
-//                }
-                
-                if !fileContent.isEmpty {
-                    Text("Conteúdo do Arquivo:")
-                        .font(.headline)
-                        .padding(.top)
-                    Text(fileContent)
-                        .padding()
-                }
             }
-            .fileImporter(isPresented: $isImporting,
-                          allowedContentTypes: [.pdf, .image]) { result in
-                switch result {
-                case .success(let url):
-                    self.fileURL = url
+            
+            //                if let fileURL = fileURL {
+            //                    Text("Arquivo Importado: \(fileURL.lastPathComponent)")
+            //                        .padding()
+            //                }
+            
+            if !fileContent.isEmpty {
+                Text("Conteúdo do Arquivo:")
+                    .font(.headline)
+                    .padding(.top)
+                Text(fileContent)
+                    .padding()
+            }
+        }
+        .fileImporter(isPresented: $isImporting,
+                      allowedContentTypes: [.pdf, .image]) { result in
+            switch result {
+            case .success(let url):
+                
+                self.fileURL = url
+                
+               _ = url.startAccessingSecurityScopedResource() // mesmo com infoplist tem que permitir aqui
+                
+                if fileURL!.pathExtension == "pdf" {
                     do {
-                        url.startAccessingSecurityScopedResource()
+                       
+                        let data = try Data(contentsOf: url)
+                        self.items.append(Item(type: 1, selectedImage: nil, pdf: fileURL!.lastPathComponent))
+                        print("Conteúdo do arquivo importado: \(data)")
+                    } catch {
+                        print("Erro ao ler o arquivo: \(error.localizedDescription)")
+                    }
+                    
+                } else { // se for qualquer tipo de imagem
+                    do {
                         let data = try Data(contentsOf: url)
                         let img = UIImage(data: data)
                         self.items.append(Item(type: 0, selectedImage: img, pdf: nil))
                     } catch {
                         print("Erro ao processar arquivo: \(error.localizedDescription)")
                     }
-                    
-                    do {
-                        let content = try read(from: url)
-                        self.fileContent = content
-                         self.items.append(Item(type: 1, selectedImage: nil, pdf: content))
-                        print("Conteúdo do arquivo importado: \(content)")
-                    } catch {
-                        print("Erro ao ler o arquivo: \(error.localizedDescription)")
-                    }
-                case .failure(let error):
-                    print("Erro ao importar o arquivo: \(error.localizedDescription)")
                 }
+                
+                
+            case .failure(let error):
+                print("Erro ao importar o arquivo: \(error.localizedDescription)")
             }
-            .navigationTitle("Detalhes")
-            .navigationBarTitleDisplayMode(.inline)
-            
-            
         }
-    }
-    
-    
-    
-    
-    func read(from url: URL) throws -> String {
-        return try String(contentsOf: url, encoding: .utf8)
+                      .navigationTitle("Detalhes")
+                      .navigationBarTitleDisplayMode(.inline)
+        
     }
 }
+
+
+
+
+func read(from url: URL) throws -> String {
+    return try String(contentsOf: url, encoding: .utf8)
+}
+
 
 
 
